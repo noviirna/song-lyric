31686a310702d40766448396b6bb0a304fb69386
Isnan Irsyad <sao.irsyad@gmail.com>
2019-06-14 09:41:03 +0700

    menambahkan backend untuk search by artis dan atau lagu

--- server/routes/thirdparty.js
+++ server/routes/thirdparty.js
@@ -1,6 +1,7 @@
 const router = require(`express`).Router();
 const controller = require('../controllers/thirdparty')
 
+router.get('/findbyparam', controller.findAlbumOrTrack)
 router.get(`/albumlist`, controller.getalbum);
 router.get(`/tracklist`, controller.gettracklist);
 router.get(`/lyric/:artist/:title`, controller.getlyric);
